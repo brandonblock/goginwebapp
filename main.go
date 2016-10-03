9dbf2fe9f76ff5441afa9f15e4daf57e91609f1b
Brandon Block <brandon.block@gmail.com>
2016-10-03 13:28:38 -0400

    articles test

--- main.go
+++ main.go
@@ -2,32 +2,21 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
+
+var router *gin.Engine
 
 func main() {
 
-	// Set router with defaults
-	router := gin.Default()
-	// Process templates when app starts
+	// Set default router
+	router = gin.Default
+
+	// Process templates at initialization
 	router.LoadHTMLGlob("templates/*")
 
-	// Define route for index
-	router.GET("/", func(c *gin.Context) {
+	// Initialize routes
+	initializeRoutes()
 
-		// Call the HTML Context method to render a template
-		c.HTML(
-			// Set the HTTP status to 200(OK)
-			http.StatusOK,
-			// Point to the index.html template
-			"index.html",
-			// Pass data to the template vars
-			gin.H{
-				"title": "Home Page",
-			},
-		)
-	})
-
-	// Start the server
+	// Start the application
 	router.Run()
 }
