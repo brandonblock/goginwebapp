d8629d5bcba9e2575ef1c01e8424592611218f53
Brandon Block <brandon.block@gmail.com>
2016-10-03 16:48:09 -0400

    test registration

--- handlers.user_test.go
+++ handlers.user_test.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"strconv"
 	"strings"
 	"testing"
 )
@@ -42,4 +43,30 @@ func TestShowRegistrationPageUnauthenticated(t *testing.T) {
 
 		return statusOK && pageOK
 	})
+}
+
+func TestRegisterUnauthenticated(t *testing.T) {
+	saveLists()
+
+	w := httptest.NewRecorder()
+	r := getRouter(true)
+
+	r.POST("/u/register", register)
+
+	registrationPayload := getRegistrationPOSTPayload()
+	req, _ := http.NewRequest("POST", "/u/register", strings.NewReader(registrationPayload))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Content-Length", strconv.Itoa(len(registrationPayload)))
+
+	r.ServeHTTP(w, req)
+
+	if w.Code != http.StatusOK {
+		t.Fail()
+	}
+
+	p, err := ioutil.ReadAll(w.Body)
+	if err != nil || strings.Index(string(p), "<title>Succesful registration &amp; Login</title>") < 0 {
+		t.Fail()
+	}
+	restoreLists()
 }
