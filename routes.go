89ecabef9ed06120a698ab3d10e98739697dc571
Brandon Block <brandon.block@gmail.com>
2016-10-04 12:30:13 -0400

    register

--- routes.go
+++ routes.go
@@ -6,6 +6,12 @@ func initializeRoutes() {
 	//Handle the index route
 	router.GET("/", showIndexPage)
 
+	userRoutes := router.Group("/")
+	{
+		userRoutes.GET("/register", showRegistrationPage)
+		userRoutes.POST("/register", register)
+	}
+
 	//Handle requests for single articles
 	router.GET("/article/view/:article_id", getArticle)
 }
