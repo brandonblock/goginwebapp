9e7df615d96604e57d3b4be9034baf3c74d9a795
Brandon Block <brandon.block@gmail.com>
2016-10-07 13:16:27 -0400

    test bad login

--- models.user_test.go
+++ models.user_test.go
@@ -2,7 +2,14 @@
 
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"strings"
+	"testing"
+)
 
 func TestUsernameAvailability(t *testing.T) {
 	saveLists()
@@ -70,4 +77,66 @@ func TestUserValidity(t *testing.T) {
 	if isUserValid("hopFrog", "1234") {
 		t.Fail()
 	}
+}
+
+func TestShowLoginPageUnathenticated(t *testing.T) {
+	r := getRouter(true)
+
+	r.GET("/u/login", showLoginPage)
+
+	req, _ := http.NewRequest("GET", "/u/login", nil)
+
+	testHTTPResponse(t, r, req, func(w *httptest.ResponseRecorder) bool {
+		statusOK := w.Code == http.StatusOK
+
+		p, err := ioutil.ReadAll(w.Body)
+		pageOK := err == nil && strings.Index(string(p), "<title>Login</title>") > 0
+
+		return statusOK && pageOK
+	})
+}
+
+func TestLoginUnauthenticated(t *testing.T) {
+	saveLists()
+	w := httptest.NewRecorder()
+	r := getRouter(true)
+
+	r.POST("/u/login", performLogin)
+
+	loginPayload := getLoginPOSTPayload()
+	req, _ := http.NewRequest("POST", "/u/login", strings.NewReader(loginPayload))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Content-Length", strconv.Itoa(len(loginPayload)))
+
+	r.ServeHTTP(w, req)
+
+	if w.Code != http.StatusOK {
+		t.Fail()
+	}
+
+	p, err := ioutil.ReadAll(w.Body)
+	if err != nil || strings.Index(string(p), "<title>Successful Login</title>") < 0 {
+		t.Fail()
+	}
+	restoreLists()
+}
+
+func TestLoginUnauthenticatedIncorrectCredentials(t *testing.T) {
+	saveLists()
+	w := httptest.NewRecorder()
+	r := getRouter(true)
+
+	r.POST("/u/login", performLogin)
+
+	loginPayload := getRegistrationPOSTPayload()
+	req, _ := http.NewRequest("POST", "/u/login", strings.NewReader(loginPayload))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Content-Length", strconv.Itoa(len(loginPayload)))
+
+	r.ServeHTTP(w, req)
+
+	if w.Code != http.StatusBadRequest {
+		t.Fail()
+	}
+	restoreLists()
 }
